82b7f93a261c65afe890a8da728b59b8ccf71c14
Yeji Choi <yechoi@student.42seoul.kr>
2020-11-26 17:54:30 +0900

    1931.py

--- greedy/1931.py
+++ greedy/1931.py
@@ -1,26 +1,18 @@
-rep = int(input())
+import sys
 
+rep = int(input())
 books = []
 for i in range(0, rep):
-    books.append(str(input()))
+    start, end = map(int, sys.stdin.readline().split())
+    books.append((start, end))
 
-books.sort()
-
-count = 0
-while len(books) != 0:
-    idx = 0
-    idx2 = 0
-    end = books[0][2]
-    for i in range(1, len(books)):
-        if books[i][2] < end:
-            end = books[i][2]
-            idx = i
-            continue
-        if books[idx][0] == books[i][0]:
-            idx = i
-    if idx < len(books)
-        books = books[idx + 1 : ]
-    else:
-        books = []
-    count += 1
+books = sorted(books, key=lambda x: (x[1], x[0]))
+count = 1
+end = books[0][1]
+i = 1
+while i < len(books):
+    if end <= books[i][0]:
+        end = books[i][1]
+        count+= 1
+    i += 1
 print(count)
