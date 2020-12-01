9f1af607cd578033a14682b435635d99588159c9
yechoi <yechoi@student.42seoul.kr>
2020-12-01 17:03:12 +0900

    7562.py

--- BFS-DFS/7562.py
+++ BFS-DFS/7562.py
@@ -1,16 +1,13 @@
-from collections import deque
 
 dx = [-2, -1, 1, 2, -2, -1, 1 ,2]
 dy = [-1, -2, -2, -1,1, 2, 2, 1]
 
-def bfs(start, end, board):
-    queue = deque()
-    queue.append((start[0], start[1]))
-    board[start[0]][start[1]] = 1
+def bfs(start, end):
+    queue = [start]
     while queue:
-        x, y = queue.popleft()
+        x, y = map(int, queue.pop(0))
         if x == end[0] and y == end[1]:
-          break;
+          return board[x][y]
         for i in range(8):
             nx = x + dx[i]
             ny = y + dy[i]
@@ -19,15 +16,14 @@ def bfs(start, end, board):
             if board[nx][ny] == 0:
                 board[nx][ny] = board[x][y] + 1
                 queue.append((nx, ny))
-    return (board[end[0]][end[1]])
 
-K = int(input())
-for _ in range(K):
+T = int(input())
+for _ in range(T):
     I = int(input())
-    ARRAY = [[0] * I] * I
-    X, Y = map(int, input().split())
-    X2, Y2 = map(int, input().split())
-    if X == X2 and Y == Y2:
+    board = [[0] * I for _ in range(I)]
+    sx, sy = map(int, input().split())
+    ex, ey = map(int, input().split())
+    if sx == ex and sy == ey:
       print(0)
     else:
-      print(bfs((X, Y), (X2, Y2), ARRAY) - 1)
+      print(bfs((sx, sy), (ex, ey)))
