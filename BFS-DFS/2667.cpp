fb9ea8888860aaeda31829cd6019cb511d1ae39f
yechoi42 <saluthuge@gmail.com>
2022-10-21 23:34:28 +0900

    feat: retry 2667.cpp

--- BFS-DFS/2667.cpp
+++ BFS-DFS/2667.cpp
@@ -1,76 +1,62 @@
 #include <iostream>
 #include <vector>
-#include <queue>
+#include <algorithm>
 #define MAX 25
-
 using namespace std;
 
 int house[MAX][MAX];
-int visited[MAX][MAX];
-int x_dir[4] = {-1, 1, 0, 0};
-int y_dir[4] = {0, 0, -1, 1};
-vector<int> result;
-queue<pair<int, int>> q;
-int cnt = 0;
+int checked[MAX][MAX];
+int dx[4] = {1, -1, 0, 0};
+int dy[4] = {0, 0, -1, 1};
+vector<int> q;
 
-void dfs(int start_x, int start_y)
+int n;
+int cnt;
+
+void dfs(int x, int y)
 {
-    visited[start_x][start_y] = 1;
-    q.push(make_pair(start_x, start_y));
-    cnt++;
-
-    while (!q.empty())
+    if (x >= 0 && x < n && y >= 0 && y < n
+    && !checked[x][y] && house[x][y])
     {
-        int x = q.front().first;
-        int y = q.front().second;
-        q.pop();
-        for(int i = 0; i < 4; i++)
+        checked[x][y] = 1;
+        cnt++;
+        for (int i = 0; i < 4; i++)
         {
-            int x_new = x + x_dir[i];
-            int y_new = y + y_dir[i];
-
-            if (maze[x_new][y_new] == 1 && !visited[x_new][y_new] &&
-            (0 <= x_new && x_new < N) && (0 <= y_new && y_new < M))
-            {
-                visited[x_new][y_new] = 1;
-                q.push(make_pair(x_new, y_new));
-                cnt++;
-            }
+            dfs(x + dx[i], y + dy[i]);
         }
     }
 }
 
 int main(void)
 {
-    int n;
-    string s;
     cin >> n;
-    for (int i = 0 ; i < n; i++)
+
+    for (int i = 0; i < n; i++)
     {
+        string s;
         cin >> s;
-        for (int j = 0; j < M; j++)
+        for (int j = 0; j < n; j++)
         {
-            house[i][j] = s[j] - '0';
+            house[i][j] = s[j] -'0';
         }
     }
-    for (int i = 0; i < n; i++) 
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++) 
+        for (int j = 0; j < n; j++)
         {
-            if (arr[i][j]=='1' && visited[i][j] == false) 
+            if (house[i][j] && !checked[i][j])
             {
                 cnt = 0;
-                bfs(i,j);
-                result.push_back(cnt);
+                dfs(i, j);
+                q.push_back(cnt);
             }
         }
     }
-    sort(result.begin(),result.end());
-    
-    cout << result.size() << "\n";
-    for (int i = 0; i < result.size(); i++) 
+    sort(q.begin(), q.end());
+    cout << q.size() << '\n';
+    for (int i = 0; i < q.size() ; i++)
     {
-        cout << result[i] << "\n";
+        cout << q[i] << '\n';
     }
     return 0;
 }
