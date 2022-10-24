977996d52d3e33ba09bf78102cab9d8eff1ce352
yechoi42 <saluthuge@gmail.com>
2022-10-24 23:29:01 +0900

    feat: add 2231.cpp

---
+++ brute_force/2231.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+    int num;
+    int sum;
+    int part;
+
+    cin >> num;
+
+    for (int i = 1; i < num; ++i)
+    {
+        sum = i;
+        part = i;
+        while (part) 
+        {
+            sum +=  part % 10;
+            part /= 10;
+        }
+        if (num == sum)
+        {
+            cout << i << endl;
+            return 0;
+        }
+    }
+    cout << "0" << endl;
+    return 0;
+}
